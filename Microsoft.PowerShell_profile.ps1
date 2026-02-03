0b226aa436caa2113faef2171453739d602d6623
hongjiaherng <jiaherng2001@gmail.com>
2026-02-04 01:01:07 +0800

    fix: update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,8 +3,6 @@ oh-my-posh init pwsh --config $HOME\.oh-my-posh.omp.json | Invoke-Expression
 $env:VIRTUAL_ENV_DISABLE_PROMPT = 1
 $env:SHELL = "C:\Program Files\PowerShell\7\pwsh.exe"
 
-$Env:UV_CACHE_DIR = "D:\jherng\System\uv\cache"
-
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
 # Be aware that if you are missing these lines from your profile, tab completion
